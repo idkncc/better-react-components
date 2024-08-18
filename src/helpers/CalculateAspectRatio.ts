0b3e175a6efdf2675decf9e56fc71b9572110117
Dmitry <58977309+Mon4ik@users.noreply.github.com>
2024-08-18 14:31:06 +0300

    fix: changed docs and added proper typechecking for `CalculateAspectRatio`

--- src/helpers/CalculateAspectRatio.ts
+++ src/helpers/CalculateAspectRatio.ts
@@ -1,11 +1,26 @@
 /**
  * Pass this function to GuiObject as ref to calculate aspect ratio
+ *
+ * @example
+ * ```tsx
+ * <Frame
+ *     size={new UDim2(.3, 0, .4, 0)}
+ *     ref={calculateAspectRatio}
+ * >
+ *     ...
+ * </Frame>
+ *
+ * In Output (some fraction):
+ * [CALC_ASPECT_RATIO](1): .875
+ * ```
  */
 export function calculateAspectRatio(element: unknown) {
-	if (element === undefined) return;
+    if (typeIs(element, "Instance") && element.IsA("GuiObject")) {
+        const sizes = (element as GuiObject).AbsoluteSize;
+        const name = (element as GuiObject).Name;
 
-	const sizes = (element as GuiObject).AbsoluteSize;
-	const name = (element as GuiObject).Name;
-
-	print(`[CALC_ASPECT_RATIO](${name}): ${sizes.X / sizes.Y}`);
+        print(`[CALC_ASPECT_RATIO](${name}): ${sizes.X / sizes.Y}`);
+    } else {
+        error(`[CALC_ASPECT_RATIO]: Passed invalid argument. GuiObject expected, got ${typeOf(element)}`)
+    }
 }
